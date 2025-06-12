40bcac28dd070d261a2924fbd08772835ef5e8a1
NinoWino <mnh01270@gmail.com>
2025-06-12 13:53:40 +0800

    .

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask, render_template
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# RENFRED PLEASE FILL IN
+# RENFRED PLEASE FILL IN
+# RENFRED PLEASE FILL IN
+# RENFRED PLEASE FILL IN
+# RENFRED PLEASE FILL IN
+# RENFRED PLEASE FILL IN
+# RENFRED PLEASE FILL IN
+# RENFRED PLEASE FILL IN
+# RENFRED PLEASE FILL IN
+DATABASE_URL = "mysql+mysqlconnector://username:password@localhost:3306/your_database"
+
+engine = create_engine(DATABASE_URL, echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 app = Flask(__name__)
 
