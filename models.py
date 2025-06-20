acd7c7df2407839c7797be4d85e0328d2b40376e
NinoWino <mnh01270@gmail.com>
2025-06-20 19:59:20 +0800

    updated mysql workbench code to add in

--- models.py
+++ models.py
@@ -8,4 +8,11 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20),default='user',nullable=False)
+    failed_attempts = db.Column(db.Integer, default=0)
+    last_failed_login = db.Column(db.DateTime)
+    is_locked = db.Column(db.Boolean, default=False)
+    two_factor_enabled = db.Column(db.Boolean, default=True, nullable=False)
+    otp_code = db.Column(db.String(6), nullable=True)
+    otp_expiry = db.Column(db.DateTime, nullable=True)
 
